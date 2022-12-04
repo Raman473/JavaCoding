36e0df56ea6839660f52091a67e0dde19e103ba0
Parasu Raman <parasuraman473@gmail.com>
2022-12-04 20:56:42 +0530

    Odd or Even Code

--- OddorEven.java
+++ OddorEven.java
@@ -1,4 +1,14 @@
 package JavaCoding;
 
+import java.util.Scanner;
+
 public class OddorEven {
+    public static void main(String[] args)
+    {
+        int value;
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter A Number");
+        value = s.nextInt();
+        System.out.println(value + " is " + (value % 2 == 0 ? "Even" : "Odd"));
+    }
 }
